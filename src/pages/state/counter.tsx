2cf5d91d0e90461c4da10c037637c632125f062d
saleh-coder <amalsalehfernandes@gmail.com>
2024-11-18 16:01:21 -0300

    Add configurable step increment and decrement to Counter component
    The Counter component now supports configurable steps, allowing users to specify the increment and decrement values. This enhances flexibility for different use cases.

--- src/pages/state/counter.tsx
+++ src/pages/state/counter.tsx
@@ -5,13 +5,22 @@ import { IconMinus, IconPlus } from "@tabler/icons-react";
 
 export default function DounterPage() {
   const [num, setNum] = useState(0);
+  const [delta, setDelta] = useState(1);
+
+  function deltaDicrease() {
+    setDelta(delta - 1);
+  }
+
+  function deltaIncrease() {
+    setDelta(delta + 1);
+  }
 
   function dicrease() {
-    setNum(num - 1);
+    setNum(num - delta);
   }
 
   function increase() {
-    setNum(num + 1);
+    setNum(num + delta);
   }
 
   return (
@@ -19,11 +28,26 @@ export default function DounterPage() {
       <div className="flex flex-col gap-5 justify-center items-center w-full h-full">
         <div className="text-7xl font-black">{num}</div>
         <div className="flex gap-5">
-          <button onClick={dicrease} className="bg-blue-500 rounded-full p-2">
+          <button onClick={dicrease} className="bg-blue-500 rounded-full p-4">
             <IconMinus size={30} />
           </button>
-          <button onClick={increase} className="bg-blue-500 rounded-full p-2">
+          <button onClick={increase} className="bg-blue-700 rounded-full p-4">
             <IconPlus size={30} />
+          </button>
+        </div>
+        <div className="flex gap-4">
+          <button
+            onClick={deltaDicrease}
+            className="flex justify-center items-center bg-purple-500 rounded-full w-9 h-9"
+          >
+            <IconMinus size={16} />
+          </button>
+          <span>{delta}</span>
+          <button
+            onClick={deltaIncrease}
+            className="flex justify-center items-center bg-purple-700 rounded-full w-9 h-9"
+          >
+            <IconPlus size={16} />
           </button>
         </div>
       </div>
