7486da0c8061fccaa55f8ce693418d538f87482e
saleh-coder <amalsalehfernandes@gmail.com>
2024-11-19 17:27:45 -0300

    feat: add InputTextPage with input field and basic state handling
    - Created a new branch `feature/input-field` for the feature implementation.
    - Added a new page `InputTextPage.tsx` inside the `state` directory to demonstrate managing state with input fields.
    - Integrated the `Page` component with a title ("Input Text Demo") and caption ("Managing State with Input Fields").
    - Styled the input field with a new `.input` class in `components.css`.
    - Linked the `InputTextPage` to the navigation menu in `Menu.tsx` using an `IconForms` icon.
    - Added an `input` element to `InputTextPage` and linked it to state using `useState` to manage the field's value dynamically.

---
+++ src/pages/state/InputTextPage.tsx
@@ -0,0 +1,17 @@
+import Page from "@/components/Page";
+import { useState } from "react";
+
+export default function InputTextPage() {
+  const [value, setValue] = useState("");
+
+  return (
+    <Page title="Input Text Demo" caption="managing Sate with Input Field">
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </Page>
+  );
+}
